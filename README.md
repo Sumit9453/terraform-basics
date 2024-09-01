6f7514eba6b90043d1080ba555b46f0f1111f238
Sumit9453 <sumit.shkl@gmail.com>
2024-09-01 14:31:10 +0530

    expense-01

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # terraform-basics
+
+```
+    $ terraform init                    ( This will intialize the plugins needed for the code ) 
+    $ terraform plan                    ( This will show what it's going to create/update/delete the infra based on the code vs what we have on the cloud )
+    $ terraform apply -auto-approve     ( This is going to apply the changes shown on the plan, if applied immediately )
+
+```
---
+++ expense-v1/ec2.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "frontend" {
+    ami = "ami-0fcc78c828f981df2"
+    instance_type = "t2.micro"
+
+    tags = {
+      Name = "frontend"
+    }
+}
+
+resource "aws_instance" "backend" {
+    ami = "ami-0fcc78c828f981df2"
+    instance_type = "t2.micro"
+
+    tags = {
+      Name = "backend"
+    }
+}
+
+resource "aws_instance" "mysql" {
+    ami = "ami-0fcc78c828f981df2"
+    instance_type = "t2.micro"
+
+    tags = {
+      Name = "mysql"
+    }
+}
